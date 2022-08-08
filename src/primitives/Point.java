5a0447e67703e999e8bd1baf5a0e4da16f356aec
Sarel <sarelsch@gmail.com>
2022-08-08 23:41:58 +0300

    Init Point

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,2 +1,54 @@
-package primitives;public class Point {
+package primitives;
+
+import java.util.Objects;
+
+/**
+ * This class will serve all primitive classes by basic point methods
+ */
+
+public class Point {
+    /**
+     * Immutable field which holds three-dimensional point
+     */
+    final Double3 xyz;
+
+    public Point(double x, double y, double z) {
+        this.xyz = new Double3(x, y, z);
+    }
+
+    public Point(Double3 xyz) {
+        this.xyz = new Double3(xyz.d1, xyz.d2, xyz.d3);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return xyz.equals(point.xyz);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" + "xyz=" + xyz + '}';
+    }
+
+    public Point add(Point point) {
+        return new Point(this.xyz.add(point.xyz));
+    }
+
+    public Vector subtract(Point point){
+        return new Vector(this.xyz.subtract(point.xyz));
+    }
+
+    public double distanceSquared(Point point){
+        return  (this.xyz.d1 - point.xyz.d1) * (this.xyz.d1 - point.xyz.d1) +
+                (this.xyz.d2 - point.xyz.d2) * (this.xyz.d2 - point.xyz.d2) +
+                (this.xyz.d3 - point.xyz.d3) * (this.xyz.d3 - point.xyz.d3);
+    }
+
+    public double distance(Point point){
+        return Math.sqrt(this.distanceSquared(point));
+    }
 }
+
