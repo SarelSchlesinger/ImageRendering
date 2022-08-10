09eca2d61ace0516e5da7109d710d1dcf77f6ba8
Sarel <sarelsch@gmail.com>
2022-08-10 14:27:59 +0300

    first tests

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,34 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTests {
+
+    Point p1 = new Point(1,2,3);
+    Point p2 = new Point(10,11,12);
+    Vector v = new Vector(1,2,3);
+
+    @Test
+    void testAdd() {
+        assertEquals(p1.add(new Point(9,9,9)),p2,"add() is incorrect");
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(v, p2.subtract(new Point(9, 9, 9)), "subtract() is incorrect");
+        assertThrows(IllegalArgumentException.class, () -> p1.subtract(p1).zeroVector(),
+                "vector (0,0,0) does not throw an exception");
+    }
+
+    @Test
+    void testDistanceSquared() {
+        assertEquals(243,p1.distanceSquared(p2),"distanceSquared() is incorrect");
+    }
+
+    @Test
+    void testDistance() {
+        assertEquals(Math.sqrt(243),p1.distance(p2),"distanceSquared() is incorrect");
+    }
+}
---
+++ unittests/primitives/VectorTest.java
@@ -0,0 +1,58 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+import static primitives.Util.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+
+    Vector v1 = new Vector(1,2,3);
+    Vector v2 = new Vector(2,4,6);
+
+    @Test
+    void VectorTest() {
+        assertThrows(IllegalArgumentException.class, () -> new Vector(0,0,0).zeroVector(),"vector can't be (0,0,0)");
+        assertThrows(IllegalArgumentException.class, () -> new Vector(new Double3(0,0,0)).zeroVector(),"vector can't be (0,0,0)");
+    }
+
+    @Test
+    void testAdd() {
+        assertEquals(v2,v1.add(v1),"add() is incorrect");
+        assertThrows(IllegalArgumentException.class, () -> v1.add(new Vector(-1,-2,-3)).zeroVector(),
+                "vector (0,0,0) does not throw an exception");
+    }
+
+    @Test
+    void testScale() {
+        assertEquals(v2,v1.scale(2),"scale() is incorrect");
+        assertThrows(IllegalArgumentException.class, () -> v1.scale(0),"vector (0,0,0) does not throw an exception");
+    }
+
+    @Test
+    void testDotProduct() {
+        assertEquals(28,v1.dotProduct(v2),"dotProduct() is incorrect");
+    }
+
+    @Test
+    void testCrossProduct() {
+        assertEquals(new Vector(0,0,0),v1.crossProduct(v2),"crossProduct() is incorrect");
+        assertThrows(IllegalArgumentException.class, () -> v1.crossProduct(v2).zeroVector(),"vector (0,0,0) does not throw an exception");
+    }
+
+    @Test
+    void testLengthSquared() {
+        assertEquals(14,v1.lengthSquared(),"lengthSquared() is incorrect");
+
+    }
+
+    @Test
+    void testLength() {
+        assertEquals(Math.sqrt(14),v1.length(),"length() is incorrect");
+    }
+
+    @Test
+    void testNormalize() {
+        assertEquals(new Vector(1 / v1.length(),2 / v1.length(),3 / v1.length()),v1.normalize(),"normalize() is incorrect");
+    }
+
+}
