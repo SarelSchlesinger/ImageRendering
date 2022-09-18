db75e4dddd048485f198bbbf29dd1c98f2220f9d
Sarel <sarelsch@gmail.com>
2022-09-18 20:38:37 +0300

    fix bugs in findIntersections() method

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -11,8 +11,9 @@ import static primitives.Util.isZero;
 
 public class Triangle extends Polygon {
 
-    public Triangle(Point... vertices) {
-        super(vertices);
+
+    public Triangle(Point p1, Point p2, Point p3) {
+        super(p1, p2, p3);
     }
 
     @Override
@@ -26,28 +27,39 @@ public class Triangle extends Polygon {
         Vector n2 = v2.crossProduct(v3).normalize();
         Vector n3 = v3.crossProduct(v1).normalize();
 
-        if (ray.getDirection().dotProduct(n1) <= 0 ||
-                ray.getDirection().dotProduct(n2) <= 0 ||
-                ray.getDirection().dotProduct(n3) <= 0) {
-            return null;
-        }
+        if ((ray.getDirection().dotProduct(n1) < 0 &&
+                ray.getDirection().dotProduct(n2) < 0 &&
+                ray.getDirection().dotProduct(n3) < 0) ||
+                (ray.getDirection().dotProduct(n1) > 0 &&
+                        ray.getDirection().dotProduct(n2) > 0 &&
+                        ray.getDirection().dotProduct(n3) > 0)) {
 
-        double numerator = this.plane.getNormal().dotProduct(this.plane.getP0().subtract(ray.getP0()));
-        double denominator = this.plane.getNormal().dotProduct(ray.getDirection());
-        if (isZero(denominator)) {
-            throw new IllegalArgumentException("denominator cannot be zero");
-        }
-        double t = alignZero(numerator / denominator);
+            double numerator = this.plane.getNormal().dotProduct(this.plane.getP0().subtract(ray.getP0()));
+            double denominator = this.plane.getNormal().dotProduct(ray.getDirection());
+            if (isZero(denominator)) {
+                throw new IllegalArgumentException("denominator cannot be zero");
+            }
+            double t = alignZero(numerator / denominator);
 
-        // The ray starts from the triangle
-        if (t == 0) {
-            return null;
-        }
+            // The ray starts from the triangle
+            if (t == 0) {
+                return null;
+            }
 
-        if (t > 0) {
-            return List.of(ray.getPoint(t));
-        }
+            if (t > 0) {
+                return List.of(ray.getPoint(t));
+            }
 
+            //return null;
+        }
         return null;
+
+//        if (!(ray.getDirection().dotProduct(n1) > 0 &&
+//                ray.getDirection().dotProduct(n2) > 0 &&
+//                ray.getDirection().dotProduct(n3) > 0)) {
+//            return null;
+//        }
+
+
     }
 }
