5b84da5698f424bb6bf6b7a79abaab29f23d8eab
Sarel <sarelsch@gmail.com>
2022-12-04 20:50:18 +0200

    rotation method

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,6 +1,11 @@
 package primitives;
 
+import static java.lang.Math.sqrt;
 import static java.lang.Math.abs;
+import static java.lang.Math.pow;
+import static java.lang.Math.sin;
+import static java.lang.Math.cos;
+import static java.lang.Math.toRadians;
 
 public class Vector extends Point {
 
@@ -31,7 +36,7 @@ public class Vector extends Point {
     }
 
     /**
-     *  throw an exception if vector == (0,0,0)
+     * throw an exception if vector == (0,0,0)
      */
     public void zeroVector() {
         if (Util.isZero(this.dotProduct(new Vector(1, 1, 1)))) {
@@ -52,25 +57,25 @@ public class Vector extends Point {
     }
 
     public double dotProduct(Vector vector) {
-        return  this.xyz.d1 * vector.xyz.d1 +
+        return this.xyz.d1 * vector.xyz.d1 +
                 this.xyz.d2 * vector.xyz.d2 +
                 this.xyz.d3 * vector.xyz.d3;
     }
 
     public Vector crossProduct(Vector vector) {
-        return  new Vector( this.xyz.d2 * vector.xyz.d3 - this.xyz.d3 * vector.xyz.d2,
-                            this.xyz.d3 * vector.xyz.d1 - this.xyz.d1 * vector.xyz.d3,
-                            this.xyz.d1 * vector.xyz.d2 - this.xyz.d2 * vector.xyz.d1);
+        return new Vector(this.xyz.d2 * vector.xyz.d3 - this.xyz.d3 * vector.xyz.d2,
+                          this.xyz.d3 * vector.xyz.d1 - this.xyz.d1 * vector.xyz.d3,
+                          this.xyz.d1 * vector.xyz.d2 - this.xyz.d2 * vector.xyz.d1);
     }
 
     public double lengthSquared() {
-        return  this.xyz.d1 * this.xyz.d1 +
+        return this.xyz.d1 * this.xyz.d1 +
                 this.xyz.d2 * this.xyz.d2 +
-                this.xyz.d3 * this.xyz.d3 ;
+                this.xyz.d3 * this.xyz.d3;
     }
 
     public double length() {
-        return Math.sqrt(this.lengthSquared());
+        return sqrt(this.lengthSquared());
     }
 
     public Vector normalize() {
@@ -83,5 +88,42 @@ public class Vector extends Point {
         Vector v2 = vector.normalize();
         return abs(v1.getX()) == abs(v2.getX()) && abs(v1.getY()) == abs(v2.getY()) && abs(v1.getZ()) == abs(v2.getZ());
     }
+
+    /**
+     * method to rotate a vector by a given angle around the z-axis
+     *
+     * @param angle - the angle of rotation
+     * @return a new rotated vector
+     */
+    public Vector rotationAroundZAxis(double angle) {
+        double radian = toRadians(angle);
+        return new Vector(this.getX() * cos(radian) - this.getY() * sin(radian) + 0,
+                          this.getX() * sin(radian) + this.getY() * cos(radian) + 0,
+                          0 + 0 + this.getZ());
+    }
+
+
+    /**
+     * method to rotate a vector by a given angle around an arbitrary axis.
+     * this calculation is based on https://mathworld.wolfram.com/RodriguesRotationFormula.html
+     *
+     * @param axis  - the axis of the rotation
+     * @param angle - the angle of rotation
+     * @return a new rotated vector
+     */
+    public Vector rotationAroundArbitraryAxis(Vector axis, double angle) {
+        double cos_angle = cos(toRadians(angle));
+        double sin_angle = sin(toRadians(angle));
+        return new Vector(this.getX() * (cos_angle + pow(axis.getX(), 2) * (1 - cos_angle))
+                                  + this.getY() * (axis.getX() * axis.getY() * (1 - cos_angle) - axis.getZ() * sin_angle)
+                                  + this.getZ() * (axis.getY() * sin_angle + axis.getX() * axis.getZ() * (1 - cos_angle)),
+                          this.getX() * (axis.getZ() * sin_angle + axis.getX() * axis.getY() * (1 - cos_angle))
+                                  + this.getY() * (cos_angle + pow(axis.getY(), 2) * (1 - cos_angle))
+                                  + this.getZ() * (-axis.getX() * sin_angle + axis.getY() * axis.getZ() * (1 - cos_angle)),
+                          this.getX() * (-axis.getY() * sin_angle + axis.getX() * axis.getZ() * (1 - cos_angle))
+                                  + this.getY() * (axis.getX() * sin_angle + axis.getY() * axis.getZ() * (1 - cos_angle))
+                                  + this.getZ() * (cos_angle + pow(axis.getZ(), 2) * (1 - cos_angle)));
+    }
+
 
 }
--- unittests/primitives/VectorTest.java
+++ unittests/primitives/VectorTest.java
@@ -3,52 +3,72 @@ package primitives;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.Math.sqrt;
-import static primitives.Util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class VectorTest {
 
-    Vector v1 = new Vector(1,2,3);
-    Vector v2 = new Vector(2,4,6);
+    Vector v1 = new Vector(1, 2, 3);
+    Vector v2 = new Vector(2, 4, 6);
 
     @Test
     void testAdd() {
-        assertEquals(v2,v1.add(v1),"add() is incorrect");
-        assertThrows(IllegalArgumentException.class, () -> v1.add(new Vector(-1,-2,-3)).zeroVector(),
-                "vector (0,0,0) does not throw an exception");
+        assertEquals(v2, v1.add(v1), "add() is incorrect");
+        assertThrows(IllegalArgumentException.class, () -> v1.add(new Vector(-1, -2, -3)).zeroVector(),
+                     "vector (0,0,0) does not throw an exception");
     }
 
     @Test
     void testScale() {
-        assertEquals(v2,v1.scale(2),"scale() is incorrect");
-        assertThrows(IllegalArgumentException.class, () -> v1.scale(0),"vector (0,0,0) does not throw an exception");
+        assertEquals(v2, v1.scale(2), "scale() is incorrect");
+        assertThrows(IllegalArgumentException.class, () -> v1.scale(0), "vector (0,0,0) does not throw an exception");
     }
 
     @Test
     void testDotProduct() {
-        assertEquals(28,v1.dotProduct(v2),"dotProduct() is incorrect");
+        assertEquals(28, v1.dotProduct(v2), "dotProduct() is incorrect");
     }
 
     @Test
     void testCrossProduct() {
-        assertEquals(new Vector(0,0,0),v1.crossProduct(v2),"crossProduct() is incorrect");
-        assertThrows(IllegalArgumentException.class, () -> v1.crossProduct(v2).zeroVector(),"vector (0,0,0) does not throw an exception");
+        assertEquals(new Vector(0, 0, 0), v1.crossProduct(v2), "crossProduct() is incorrect");
+        assertThrows(IllegalArgumentException.class, () -> v1.crossProduct(v2).zeroVector(), "vector (0,0,0) does not throw an exception");
     }
 
     @Test
     void testLengthSquared() {
-        assertEquals(14,v1.lengthSquared(),"lengthSquared() is incorrect");
+        assertEquals(14, v1.lengthSquared(), "lengthSquared() is incorrect");
 
     }
 
     @Test
     void testLength() {
-        assertEquals(sqrt(14),v1.length(),"length() is incorrect");
+        assertEquals(sqrt(14), v1.length(), "length() is incorrect");
     }
 
     @Test
     void testNormalize() {
-        assertEquals(new Vector(1 / v1.length(),2 / v1.length(),3 / v1.length()),v1.normalize(),"normalize() is incorrect");
+        assertEquals(new Vector(1 / v1.length(), 2 / v1.length(), 3 / v1.length()), v1.normalize(), "normalize() is incorrect");
     }
 
+    @Test
+    void testRotationAroundZAxis() {
+        assertEquals(new Vector(-2, 2, 5), new Vector(2, 2, 5).rotationAroundZAxis(90), "rotationAroundZAxis() is incorrect");
+    }
+
+    @Test
+    void testRotationAroundArbitraryAxis() {
+
+        // TC1: Rotation around x-axis
+        assertEquals(new Vector(0, 0, 5), new Vector(0, 0, -5).rotationAroundArbitraryAxis(new Vector(1, 0, 0), 180), "rotationAroundArbitraryAxis() is incorrect");
+        assertEquals(new Vector(0, 0, 0), new Vector(0, 0, 0).rotationAroundArbitraryAxis(new Vector(1, 0, 0), 180), "rotationAroundArbitraryAxis() is incorrect");
+        // TC2: Rotation around y-axis
+        assertEquals(new Vector(0, 0, 1), new Vector(0, 0, -1).rotationAroundArbitraryAxis(new Vector(0, 1, 0), 180), "rotationAroundArbitraryAxis() is incorrect");
+        assertEquals(new Vector(0, 0, 0), new Vector(0, 0, 0).rotationAroundArbitraryAxis(new Vector(0, 1, 0), 180), "rotationAroundArbitraryAxis() is incorrect");
+        // TC3: Rotation around z-axis
+        assertEquals(new Vector(-2, 2, 5), new Vector(2, 2, 5).rotationAroundArbitraryAxis(new Vector(0, 0, 1), 90), "rotationAroundArbitraryAxis() is incorrect");
+        assertEquals(new Vector(0, 0, 0), new Vector(0, 0, 0).rotationAroundArbitraryAxis(new Vector(0, 0, 1), 180), "rotationAroundArbitraryAxis() is incorrect");
+        // TC4: Rotation around arbitrary axis
+        assertEquals(new Vector(4, 4, 1), new Vector(2, 2, 5).rotationAroundArbitraryAxis(new Vector(1, 1, 1).normalize(), 180), "rotationAroundArbitraryAxis() is incorrect");
+        assertEquals(new Vector(5, 2, 2), new Vector(2, 2, 5).rotationAroundArbitraryAxis(new Vector(1, 1, 1).normalize(), 120), "rotationAroundArbitraryAxis() is incorrect");
+    }
 }
